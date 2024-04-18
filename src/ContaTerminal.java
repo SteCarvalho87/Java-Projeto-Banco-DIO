e66fb02da8072d6ec2763746d9795bd7cb9e9ffb
Stephanie Carvalho <ste.aoc@gmail.com>
2024-04-18 18:26:08 -0300

    update do Reame e conclusão do desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        
+        int numeroConta;       
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Olá cliente! por favor, digite o numero da Agencia");
+        agencia = scan.next();
+
+        System.out.println("Agora, digite o numero da sua Conta Bancária");
+        numeroConta = scan.nextInt();
+
+        System.out.println("Digite também o seu nome completo");
+        nomeCliente = scan.nextLine();
+
+        System.out.println("Informe o saldo");
+        saldo = scan.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + "! Obrigado por criar uma conta em nosso banco!\nSua agência é: " + agencia + "\nConta:" + numeroConta + "\nSeu saldo de R$" + saldo + "já está disponível para saque");
+        
     }
 }
